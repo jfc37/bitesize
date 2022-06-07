cde7c0a23b231653e02b685b9cb10f351c5edbcc
Joe Chapman <joe.chapman3737@gmail.com>
2022-06-07 21:17:29 +1200

    course selection page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'course-selection' },
+  {
+    path: 'course-selection',
+    loadChildren: () =>
+      import('./course-selection/course-selection.module').then(
+        (m) => m.CourseSelectionModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/course-selection/course-selection.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-course-selection',
+  templateUrl: './course-selection.component.html',
+  styleUrls: ['./course-selection.component.scss'],
+})
+export class CourseSelectionComponent {}
